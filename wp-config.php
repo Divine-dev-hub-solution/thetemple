453ce41117ead081d4b18264c60059d4f3e380bb
vasudev-divine <vasudev.dantani@divinedevhub.in>
2026-05-08 12:36:54 +0530

    content updates

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'srividya_user' );
 define( 'DB_PASSWORD', 'Srividya@1234' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define('DB_HOST', '127.0.0.1');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
